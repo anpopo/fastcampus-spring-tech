a9fbf975acb135812c5dc2447da0b84719f6ac50
ansehyeong <dkstpgud@gmail.com>
2022-07-06 00:23:37 +0900

    initial commit to multimodule for fast campus spring tech

---
+++ spring-framework-practice/src/main/java/anpopo/spring/framework/practice/SpringFrameworkPracticeApplication.java
@@ -0,0 +1,13 @@
+package anpopo.spring.framework.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFrameworkPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFrameworkPracticeApplication.class, args);
+	}
+
+}
